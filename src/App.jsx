a532fab2f062fc6479ffc7f1d2bba27448ad2712
anksrane <anks.rane@gmail.com>
2025-12-25 23:29:38 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import {Button} from './components';
+
+function App() {
+
+  return (
+      <div className="min-h-screen flex items-center justify-center bg-gray-900">
+        <Button />
+      </div>
+  )
+}
+
+export default App
